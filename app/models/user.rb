369fa31fa8cf0bbc2da47bc91753983926717acc
tddycks <tadzio.smith@gmail.com>
2016-10-25 20:19:13 -0400

    fixed typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   
   has_secure_password
   has_many :comments
-  has many :user_events
+  has_many :user_events
   has_many :events, through: :user_events
   validates_presence_of :email
   validates_uniqueness_of :email
