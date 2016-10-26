5392f3ea88f7d322b95ff2fec5399f59e95a1c14
tddycks <tadzio.smith@gmail.com>
2016-10-25 20:27:15 -0400

    added slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username
   validates_presence_of :password
+
+  def slug
+    self.username.downcase.gsub(" ", "-").gsub(/[^a-zA-Z0-9-]/, "")
+  end
+
+  def self.find_by_slug(slug) 
+    self.all.find {|user| user.slug == slug}
+  end
   
 
 end
