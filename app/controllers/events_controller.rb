1d71b5f4a7e3eb63627457000e5993ce77bac742
tddycks <tadzio.smith@gmail.com>
2016-10-26 21:11:21 -0400

    added login/signup forms

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,8 @@
 class EventsController < ApplicationController
 
+  get '/events' do
+    "Here are the events"
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,31 @@ class UsersController < ApplicationController
     user = User.new(params)
     if user.save 
       session[:user_id] = user.id
+      redirect "/events"
     else
       redirect "/signup"
     end
+  end
+
+  post "/login" do
+    #raise params.inspect
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/events"
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/logout" do
+    if logged_in?
+      session.clear
+      redirect "/"
+    else
+      redirect "/"
+    end
+
   end
 
 end
