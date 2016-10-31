4246d0c0283c5ff34e436eda57c4038f234967ac
tddycks <tadzio.smith@gmail.com>
2016-10-31 14:51:36 -0400

    added comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
 
 
+  post "/comments" do 
+    if logged_in?
+     current_user.comments.create(params)
+     redirect "/events/#{params[:event_id]}"
+    else
+      redirect "/"
+    end
+  end
+
+
 end
